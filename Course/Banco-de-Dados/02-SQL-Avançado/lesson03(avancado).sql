f8e0a587f498b80ee996c3f720d7209464c90247
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2022-10-12 01:40:19 +0300

    update database lesson file

--- Course/Banco-de-Dados/02-SQL-Avançado/lesson03(avancado).sql
+++ Course/Banco-de-Dados/02-SQL-Avançado/lesson03(avancado).sql
@@ -17,8 +17,24 @@ SELECT SUM(length) FROM film;
 SELECT * FROM payment;
 SELECT SUM(amount) FROM payment;
 SELECT MAX(payment_id) FROM payment;
+
 -- OU
 SELECT COUNT(amount) AS "How much amount", 
 SUM(amount) AS "sum of payment amount",
 AVG(amount) AS "average payment"
 FROM payment;
+
+-- Agrupamento de dados (GROUP BY)
+SELECT COUNT(city_id) AS "Qtd de cidades", 
+c.country AS "qtd de cidades" FROM city
+JOIN country c
+USING(country_id)
+GROUP BY(country);
+
+-- EXERCICIO2: Agrupar os filmes por categoria
+SELECT COUNT(film_id) AS "qtd de filmes", c.name FROM film
+JOIN film_category fc
+USING(film_id)
+JOIN category c
+USING(category_id)
+GROUP BY(c.name);
