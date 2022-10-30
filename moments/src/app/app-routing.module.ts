99c184f58b1c1fb22c33a2a8bbcb88aa67d483cb
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2022-10-30 14:28:37 +0300

    added header, home, about and footer components

--- moments/src/app/app-routing.module.ts
+++ moments/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Duas páginas declaradas (urls declaradas)
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '/moments/new', component: NewMomentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    HomeComponent,
+    FooterComponent,
+    NewMomentComponent
   ],
   imports: [
     BrowserModule,
