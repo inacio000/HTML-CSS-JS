4c7109692faf362d12add8964d425a02f9310528
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2022-11-13 13:57:59 +0300

    Added new lesson files (DOM)

---
+++ Course/Rocketseat/DOM/DOM.md
@@ -0,0 +1,28 @@
+# DOM (Document Object Model)
+## O que é?
+* É o HTML convertido para um objeto JavaScript
+* API que representa e interage com o HTML
+* Estrutura de dados do tipo árvore, criada pelo browser
+* Propriedades e métodos
+#
+## Para que serve?
+* JavaScript usa a DOM para se conectar ao HTML
+* Manipular o HTML com o JavaScript
+* Você só programa para WEB porque existe a DOM
+#
+## Qual usar?
+* getElementById()
+    > retorna Element
+
+* getElementByClassName()
+    > retorna HTMLCollection
+
+* getElementByTagName()
+    > retorna HTMLCollection
+
+* querySelector()
+    > retorna Element
+
+* querySelectorAll()
+    > retorna Nodelist
+    
---
+++ Course/Rocketseat/DOM/script.js
@@ -0,0 +1,37 @@
+// getElementById()
+const elementId = document.getElementById('title')
+console.log(elementId)
+
+// getElementByClassName()
+const elementClass = document.getElementsByClassName('one');
+console.log(elementClass)
+
+// getElementByTagName()
+const elementTag = document.getElementsByTagName('meta');
+console.log(elementTag)
+
+// querySelector()
+const elementQuerySelector = document.querySelector('[src');
+console.log(elementQuerySelector)
+
+// querySelectorAll()
+const elementsQuerySelectorAll = document.querySelectorAll('.one');
+console.log(elementsQuerySelectorAll)
+
+elementsQuerySelectorAll.forEach(el => console.log(el)); // Usado por causa do NodeList
+
+/*
+    Qual usar?
+
+    getElementById() -> retorna Element
+
+    getElementByClassName() -> retorna HTMLCollection
+
+    getElementByTagName() -> retorna HTMLCollection
+
+    querySelector() -> retorna Element
+
+    querySelectorAll() -> retorna Nodelist
+
+
+*/ 
