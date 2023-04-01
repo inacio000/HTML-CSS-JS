3e9783ada4863cc835fb7f5a4a4aa9e872da4620
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-04-01 04:05:15 +0300

    refactored project, added responsive layout and readme file

--- Course/Rocketseat/Challanges/Rocket-Blog/script.js
+++ Course/Rocketseat/Challanges/Rocket-Blog/script.js
@@ -7,3 +7,30 @@
 //   this.className += " active";
 //   });
 // }
+
+var burgerIcon = document.querySelector(".burger img");
+var menu = document.querySelector("ul");
+
+burgerIcon.addEventListener("click", function() {
+
+    if (burgerIcon.getAttribute("src") == "src/burger.svg") {
+        
+        burgerIcon.setAttribute("src", "src/close.svg")
+
+        if (menu.style.display === "flex") {
+            menu.style.display = "none"
+        } else {
+            menu.style.display = "flex"
+        }
+
+    } else {
+        burgerIcon.setAttribute("src", "src/burger.svg");
+
+        if (menu.style.display === "flex") {
+            menu.style.display = "none"
+        } else {
+            menu.style.display = "flex"
+        }
+    }
+
+});
